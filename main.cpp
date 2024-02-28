a6d8c3027801059bb28652dcdef946838dd387b4
Dr.ME <59316877+l-NightLord-l@users.noreply.github.com>
2024-02-28 08:05:07 +0000

    division operator has been added

--- main.cpp
+++ main.cpp
@@ -18,7 +18,11 @@ int main() {
       //code3
     break;
     case 4:
-      //code4
+      if (b == 0) {
+        std::cout << "Ділення на нуль неможливе!";
+        return 1;
+      }
+      result = a / b;
     break;
   }
   std::cout << "Результат = " << result;
