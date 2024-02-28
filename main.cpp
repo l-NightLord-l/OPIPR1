70e6736e72a404f29567764187c6635ae0ff436d
Dr.ME <59316877+l-NightLord-l@users.noreply.github.com>
2024-02-28 07:52:46 +0000

    the code has been formatted for better readability

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 
 int main() {
-  int a, b, op; double result;
+  int a, b, op;
+  double result;
 
-  std::cout << "Введіть перше число\n"; std::cin >> a;
-  std::cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n"; std::cin >> op;
-  std::cout << "Введіть друге число\n"; std::cin >> b;
+  std::cout << "Введіть перше число\n";
+  std::cin >> a;
+  std::cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
+  std::cin >> op;
+  std::cout << "Введіть друге число\n";
+  std::cin >> b;
 
-  switch(op) {
-    case 1:
-      result = a + b;
+  switch (op) {
+  case 1:
+    result = a + b;
     break;
-    case 2:
-      result = a - b;
+  case 2:
+    result = a - b;
     break;
-    case 3:
-      //code3
+  case 3:
+    // code3
     break;
-    case 4:
-      //code4
+  case 4:
+    // code4
     break;
   }
   std::cout << "Результат = " << result;
